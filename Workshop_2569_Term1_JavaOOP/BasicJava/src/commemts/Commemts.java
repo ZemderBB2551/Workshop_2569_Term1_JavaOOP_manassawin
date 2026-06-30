e7e522c77a385f96cd539de7267d337e1c88c8d5
ZemderBB2551 <manassawinlg@gmail.com>
2026-06-30 12:04:48 +0700

    Add files via upload

---
+++ Workshop_2569_Term1_JavaOOP/BasicJava/src/commemts/Commemts.java
@@ -0,0 +1,22 @@
+package commemts;
+
+public class Commemts {
+    /**
+     * 3.document line comment
+     *
+     */
+    void add()
+    {
+        System.out.print("add comment");
+    }
+    public static void mian(String[] args){
+        int a; // 1.single line comment
+
+        /*
+        int age;
+        Sting name;
+        Sting address;
+        2.multi line comment
+         */
+    }
+}
